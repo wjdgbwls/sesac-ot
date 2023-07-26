db159901710a4fe64af23de9678aff98b52f39bf
wjdgbwls <iwjdgbwls@gmail.com>
2023-07-26 15:34:22 +0900

    [수정] item sql쿼리문을 삭제하고 ORM을 사용해서 다시 작성

--- data_generator/blueprints/item_blueprint.py
+++ data_generator/blueprints/item_blueprint.py
@@ -1,20 +1,12 @@
-import sqlite3
-import filepath
 from flask import Blueprint, render_template
-import csv
 
+from models import Item
 item_bp = Blueprint('item', __name__)
 
 @item_bp.route('/')
 def orderitem():
     data=[]
-    conn = sqlite3.connect('./dbfile/user-sample.db')
-    cursor = conn.cursor()
-    datas = cursor.fetchall()
-    # SQL 쿼리 작성
-    query = "SELECT * FROM items WHERE 1"
-    cursor.execute(query)
-    datas = cursor.fetchall()
-    for i in datas:
-        data.append(i)
-    return render_template("item.html", itemname = data)
+    all_stores = Item.query.all()
+    print(all_stores)
+    return render_template("item.html", stores=all_stores)
+    
