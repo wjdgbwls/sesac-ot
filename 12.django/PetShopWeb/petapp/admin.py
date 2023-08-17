586d057ab767d3708c59c7017e1b0d6e814080dc
wjdgbwls <iwjdgbwls@gmail.com>
2023-08-17 21:52:09 +0900

    [생성] mainimg DB생성

--- 12.django/PetShopWeb/petapp/admin.py
+++ 12.django/PetShopWeb/petapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import User
 from .models import Item
+from .models import MainImg
+
 admin.site.register(User)
 admin.site.register(Item)
+admin.site.register(MainImg)
